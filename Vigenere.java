80034538f3bdf5d4c8077fdf82f0293ebdc8aead
gyash <yxg407@case.edu>
2020-11-28 08:22:26 -0500

    Added png functionality and fixed txt issues.

--- Vigenere.java
+++ Vigenere.java
@@ -6,13 +6,9 @@ public class Vigenere {
     public static void main(String[] args) throws IOException {
 
         VigenereCipher vc;
-        //Message if improper arguments are given
-            /*if (args.length != 3) {
-                System.out.println("Usage: Vigenere plaintext_file ciphertext_file key_file");
-                return;
-            }*/
-        String input = "text";
-        String key = "key.txt";
+
+        String input = "jssus.png";
+        String key = "key";
         String output = "output";
         vc = new VigenereCipher(input, output, key);
         vc.execute();
